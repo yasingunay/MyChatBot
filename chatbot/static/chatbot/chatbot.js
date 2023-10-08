59e8b26ea61b58ba59c6fb8b6592975b7fdcd1c2
yasingunay <av.yasingunay@gmail.com>
2023-10-08 10:15:55 +0200

    bard api added

---
+++ chatbot/static/chatbot/chatbot.js
@@ -0,0 +1,59 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const messagesList = document.querySelector(".messages-list");
+    const messageForm = document.querySelector(".message-form");
+    const messageInput = document.querySelector(".message-input");
+
+
+    messageForm.addEventListener('submit', (event) => {
+        // prevent the default form behavior
+        event.preventDefault();
+
+        // delete whitespace from the message
+        const message = messageInput.value.trim();
+        if (message.length === 0){
+            return;
+        }
+
+        const messageItem = document.createElement("li");
+        messageItem.classList.add('message', 'sent');
+        messageItem.innerHTML = `
+        <div class="message-text">
+            <div class="message-sender">
+              <b>You</b>
+            </div>
+            <div class="message-content">
+              ${message}
+            </div>
+          </div>`;
+        messagesList.appendChild(messageItem);
+
+        messageInput.value ="";
+
+        // send the message to the server
+        fetch("",{
+            method: "POST",
+            headers: {"Content-Type": "application/x-www-form-urlencoded"},
+            body: new URLSearchParams({
+                "csrfmiddlewaretoken": document.querySelector('[name="csrfmiddlewaretoken"]').value,
+                "message": message
+            })
+        })
+        .then(response => response.json())
+        .then(data =>{
+            const response = data.response;
+            console.log(data);
+            const messageItem = document.createElement("li");
+            messageItem.classList.add('message', 'received');
+            messageItem.innerHTML = `
+            <div class="message-text">
+            <div class="message-sender">
+                <b>You</b>
+                </div>
+                <div class="message-content">
+                ${response}
+             </div>
+            </div>`;
+        messagesList.appendChild(messageItem);
+        })
+    })
+});
--- chatbot/views.py
+++ chatbot/views.py
@@ -1,13 +1,28 @@
+import requests
+import os
+from bardapi import Bard
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
+
+api_key = os.environ.get("BARD_API_KEY")
+
+bard = Bard(token=api_key)
+
+
+
 # Create your views here.
+
+def ask_bard(message):
+    response = bard.get_answer(message)
+    answer = response["content"]
+    return answer
+
 
 def index(request):
     if request.method == "POST":
         message = request.POST.get("message")
-        print(message)
-        response = "Hello, world!"
+        response = ask_bard(message)
         return JsonResponse({"message": message, "response": response})
     return render(request, "chatbot/chatbot.html")

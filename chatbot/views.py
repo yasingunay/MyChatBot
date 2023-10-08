4fec92464e2e5ca237e43af3ae0c2ccec653a667
yasingunay <av.yasingunay@gmail.com>
2023-10-08 11:22:53 +0200

    dotenv added

--- chatbot/views.py
+++ chatbot/views.py
@@ -3,18 +3,26 @@ import os
 from bardapi import Bard
 from django.shortcuts import render
 from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
+from dotenv import load_dotenv
 
+load_dotenv()
+token = os.getenv("BARD_API_KEY")
 
-api_key = os.environ.get("BARD_API_KEY")
-
-bard = Bard(token=api_key)
+bard = Bard(token=token)
 
 
 
 # Create your views here.
-
 def ask_bard(message):
+    """
+    Returns the answer obtained from the bard.get_answer method.
+
+    Args:
+        message (str): The message to be passed to the bard.get_answer method.
+
+    Returns:
+        str: The content of the response obtained from the bard.get_answer method.
+    """
     response = bard.get_answer(message)
     answer = response["content"]
     return answer
@@ -26,3 +34,6 @@ def index(request):
         response = ask_bard(message)
         return JsonResponse({"message": message, "response": response})
     return render(request, "chatbot/chatbot.html")
+
+# def login(request):
+#     return render(request, "chatbot/login.html")
